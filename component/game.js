ac4dc958289f1f32c7fdcf4fb2fd7e52e29a95e6
Andrii Nyzhnyk <andrij1510@gmail.com>
2018-05-03 00:36:48 +0300

    add function createElement

--- component/game.js
+++ component/game.js
@@ -2,17 +2,28 @@ import React from 'react'
 import Item from './item'
 
 const Game = ({items, itemClick}) => (
-    <div id="game">
-        {
-            items.map(item => <Item
-                    key={item}
-                    src={item}
-                    onClick={() => itemClick()}
-                />
-            )
-        }
-    </div>
+    <div id="game">{createElement(items, itemClick)}</div>
 
 );
 
+function createElement( items, itemClick) {
+    let res = new Array(items.length);
+    for(let i = 0; i < items.length; i++) {
+        res.push(<Item
+                    key={items[i]}
+                    src={items[i]}
+                    onClick={() => itemClick()}
+                />
+        );
+    }
+    return res;
+}
+
 export default Game;
+
+// items.map(item => <Item
+//         key={item}
+//         src={item}
+//         onClick={() => itemClick()}
+//     />
+// )
