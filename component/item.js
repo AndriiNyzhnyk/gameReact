a898d465e13f81902b70827bbb5a385674fed858
Andrii Nyzhnyk <andrij1510@gmail.com>
2018-05-09 17:54:59 +0300

    rewrite item.js(optimize create img)

--- component/item.js
+++ component/item.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
-const Item = ({src, id, onClick}) => (
-    <div className="items" id={id} onClick={onClick}>
-        <img src={src}></img>
-    </div>
-);
+const Item = ({src, id, onClick}) => {
+    console.log(typeof src);
+    if(src !== undefined) {
+        return (
+            <div className="items" id={id} onClick={onClick}>
+                <img src={src}></img>
+            </div>
+        );
+    } else {
+        return (
+            <div className="items" id={id} onClick={onClick}></div>
+        );
+    }
+
+};
 
 export default Item;
